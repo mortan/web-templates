98404694ca04fea129b749635c31cead9625896f
U-Gam-PC\Gam <lenz_fabian@yahoo.de>
2012-10-31 11:56:16 +0100

    Library update

--- knockout-requirejs/js/main.js
+++ knockout-requirejs/js/main.js
@@ -1,7 +1,7 @@
 require.config({
 	paths: {
-		'jquery': 'lib/jquery-1.8.1.min',
-		'knockout': 'lib/knockout-2.1.0',
+		'jquery': 'lib/jquery-1.8.2.min',
+		'knockout': 'lib/knockout-2.2.0',
 		'domReady': 'lib/domReady'
 	},
 	shim: {
