765aced5ea0008e4628b222f997ef98d01a45ca5
Fabian Lenz <lenz_fabian@yahoo.de>
2012-11-27 22:34:45 +0100

    Added Jasmine.js sample project

---
+++ jasmine/scripts/index.js
@@ -0,0 +1,9 @@
+(function() {
+	var self = this;
+
+	self.addOne = function(n) {
+		return n + 1;
+	};
+
+	return self;
+})();
---
+++ jasmine/tests/spec/index.js
@@ -0,0 +1,5 @@
+describe("index", function() {
+	it("should add one", function() {
+		expect(addOne(0)).toEqual(1);
+	});
+});
