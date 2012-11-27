dbbef4a301742bc94edaf0e58b1426bf226bf1da
Fabian Lenz <lenz_fabian@yahoo.de>
2012-11-27 20:18:03 +0100

    Updated jQuery to 1.8.3

--- full-stack/scripts/app.js
+++ full-stack/scripts/app.js
@@ -2,7 +2,7 @@ require.config({
 	baseUrl: './scripts',
 
 	paths: {
-		'jquery': 'lib/jquery-1.8.2.min',
+		'jquery': 'lib/jquery-1.8.3.min',
 		'knockout': 'lib/knockout-2.2.0',
 		'domReady': 'lib/domReady'
 	},
--- knockout-requirejs/scripts/main.js
+++ knockout-requirejs/scripts/main.js
@@ -1,6 +1,6 @@
 require.config({
 	paths: {
-		'jquery': 'lib/jquery-1.8.2.min',
+		'jquery': 'lib/jquery-1.8.3.min',
 		'knockout': 'lib/knockout-2.2.0',
 		'domReady': 'lib/domReady'
 	},
